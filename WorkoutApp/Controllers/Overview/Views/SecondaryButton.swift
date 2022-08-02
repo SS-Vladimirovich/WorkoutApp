3dc08bc59f33332bbcbab48d1e03f430889278a0
Sergey Sokolov <sv.sokolov716@gmail.com>
2022-08-02 17:33:28 +0300

    Create Button custom

---
+++ WorkoutApp/Controllers/Overview/Views/SecondaryButton.swift
@@ -0,0 +1,67 @@
+//
+//  SecondaryButton.swift
+//  WorkoutApp
+//
+//  Created by SERGEY SOKOLOV on 02.08.2022.
+//
+
+import Foundation
+import UIKit
+
+class SecondaryButton: UIButton {
+    
+    let label = UILabel()
+    let iconView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addViews()
+        layoutViews()
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setTitle(_ title: String) {
+        label.text = title
+    }
+}
+
+extension SecondaryButton {
+    
+    func addViews() {
+        addSubview(label)
+        addSubview(iconView)
+    }
+    
+    func layoutViews() {
+        NSLayoutConstraint.activate([
+            iconView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            iconView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            iconView.heightAnchor.constraint(equalToConstant: 5),
+            iconView.widthAnchor.constraint(equalToConstant: 10),
+            
+            label.centerYAnchor.constraint(equalTo: centerYAnchor),
+            label.trailingAnchor.constraint(equalTo: iconView.leadingAnchor),
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10)
+        ])
+    }
+    
+    func configure() {
+        backgroundColor = Resouces.Colors.secondary
+        layer.cornerRadius = 14
+        makeSystem(self)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = Resouces.Colors.active
+        label.textAlignment = .center
+        label.font = Resouces.Fonts.helvelticaRegular(with: 15)
+        
+        iconView.translatesAutoresizingMaskIntoConstraints = false
+        iconView.image = Resouces.Images.Common.downArrow?.withRenderingMode(.alwaysTemplate)
+        iconView.tintColor = Resouces.Colors.active
+    }
+}
--- WorkoutApp/Helpers/Resouces.swift
+++ WorkoutApp/Helpers/Resouces.swift
@@ -13,8 +13,10 @@ enum Resouces {
         static let inactive = UIColor(hexString: "#929DA5")
         
         static let separator = UIColor(hexString: "#E8ECEF")
-        static let titleGray = UIColor(hexString: "#545C77")
         static let backgraund = UIColor(hexString: "#F8F9F9")
+        static let secondary = UIColor(hexString: "#F0F3FF")
+        
+        static let titleGray = UIColor(hexString: "#545C77")
     }
     
     enum Strings {
@@ -33,11 +35,19 @@ enum Resouces {
             static let progress = UIImage(named: "progress_tab")
             static let settings = UIImage(named: "settings_tab")
         }
+        
+        enum Common {
+            static let downArrow = UIImage(named: "down_arrow")
+        }
     }
     
     enum Fonts {
         static func helvelticaRegular(with size: CGFloat) -> UIFont {
             UIFont(name: "Helvetica", size: size) ?? UIFont()
         }
+    }
+    
+    enum Overview {
+        static let allWorkoutsButton = "All Workouts"
     }
 }
